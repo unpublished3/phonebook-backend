4638fcccb6a48589002b7cfecb3da348a78aae30
unpublished3 <chapagainsakshyam@gmail.com>
2024-10-01 12:31:24 +0545

    Complete 3.8

--- index.js
+++ index.js
@@ -3,9 +3,21 @@ const uuid = require("uuid");
 const morgan = require("morgan");
 
 const app = express();
+const logger = morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, "content-length"),
+    "-",
+    tokens["response-time"](req, res),
+    "ms",
+    tokens.method(req, res) === "POST" ? JSON.stringify(req.body) : "",
+  ].join(" ");
+});
 
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(logger);
 
 let persons = [
   {
